8194f5edb7ef41fbd00d3f824656310c4836592d
unknown <you@example.com>
2023-02-16 14:15:21 +0100

    rendu

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connexion à la base de données MySQL
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "gestion_taches"
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données MySQL : ", err);
+  } else {
+    console.log("Connexion à la base de données MySQL réussie !");
+  }
+});
+
+// Routes
+const tasksRouter = require("./routes/tasks");
+app.use("/tasks", tasksRouter);
+
+const projectsRouter = require("./routes/projects");
+app.use("/pojects", projectsRouter);
+
+// Lancement du serveur
+app.listen(port, () => {
+  console.log(`Serveur lancé sur le port ${port}`);
+});
